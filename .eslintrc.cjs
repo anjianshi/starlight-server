349f0ffcd96aa91ff6e9947148331207062b0941
anjianshi <anjianshi@gmail.com>
2023-03-06 18:47:43 +0800

    initial project layout

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    './node_modules/@anjianshi/presets/eslint-base.js',
+    './node_modules/@anjianshi/presets/eslint-node.js',
+    './node_modules/@anjianshi/presets/eslint-typescript.js',
+  ],
+}
